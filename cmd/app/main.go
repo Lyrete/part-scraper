c5de505421c55d7b039390c6ff37b4826275afcf
Tommi Alajoki <tommi.d.alajoki@utu.fi>
2023-09-27 20:31:21 +0300

    Add Bike-Discount + make the searching stable-ish

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/go-rod/rod"
+	"github.com/lyrete/part-scraper/internal/bdiscount"
 	"github.com/lyrete/part-scraper/internal/r2"
 )
 
@@ -12,13 +13,18 @@ func main() {
 	browser := rod.New().MustConnect()
 	defer browser.MustClose()
 
+	r2Tab := r2.Connect(browser)
+	bdTab := bdiscount.Connect(browser)
+
 	fmt.Println("Loop search for a few items...")
 
 	itemsToSearch := []string{"SRAM XG-1275", "SRAM XG-1295", "GX Eagle Chain", "DT Swiss GR1600"}
 
 	for _, itemName := range itemsToSearch {
-		item := r2.SearchItemByName(browser, itemName)
-		fmt.Println(item)
+		item := r2.SearchItemByName(r2Tab, itemName)
+		bd_item := bdiscount.SearchItemByName(bdTab, itemName)
+		fmt.Println("B-D:", bd_item)
+		fmt.Println("R2:", item)
 	}
 
 	browser.Close()
